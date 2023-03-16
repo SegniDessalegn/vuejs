3fb91336dff9fcdee6c89700836ac508c114b71e
Segni Dessalegn <segnides@gmail.com>
2023-03-16 11:41:06 +0300

    conditional rendering

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const app = Vue.createApp({
         return {
             title: "the final empire",
             author: "ME",
-            age: 45
-      }
+            age: 45,
+            showbooks: true,
+        };
     },
 
     methods: {
         changeTitle(title){
             this.title = title
+        },
+        toggleShowBooks(){
+            this.showbooks = !this.showbooks
         }
     }
 })
